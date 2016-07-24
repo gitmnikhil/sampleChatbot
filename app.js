671aab6fb289aae5a4e0cf5f80edac88657c0dfc
Nikhil Mittal <nikhilmittalece@gmail.com>
2016-07-24 19:37:05 +0530

    updating

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3798, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "63bf713a-ea95-495a-a285-67c0cdec5813",
+    appPassword: "bPSLoYd955CDhynX6pViPq7"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
