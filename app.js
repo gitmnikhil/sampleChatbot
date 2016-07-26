472f70c42f2a4513c4a21cd57c21b83bb527746f
Nikhil Mittal <nikhilmittalece@gmail.com>
2016-07-26 08:26:58 +0530

    updating providing support for updating recommendation

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ server.listen(process.env.port || process.env.PORT || 3798, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "63bf713a-ea95-495a-a285-67c0cdec5813",
-    appPassword: "bPSLoYd955CDhynX6pViPq7"
+    //appId: "63bf713a-ea95-495a-a285-67c0cdec5813",
+    //appPassword: "bPSLoYd955CDhynX6pViPq7"
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -33,23 +35,43 @@ intents.matches(/^change name/i, [
 
 intents.onDefault([
     function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        session.send('Hello %s!', session.userData.name);
+        session.beginDialog('/update');
     }
 ]);
 
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+bot.dialog('/update', [
+    function (session, args, next) {
+        if(session.dialogData.reAskuserResponse){
+            session.dialogData.reAskuserResponse = false;
+            builder.Prompts.text(session, 'I am sorry, I could not understand. Do you want an upgrade recommendation? Yes/No');    
+        }else{
+            builder.Prompts.text(session, 'Welcome to opsfaq! Do you want an upgrade recommendation? Yes/No');    
+        }
+    },
+    function (session,results) {
+        if(results.response.toLowerCase().indexOf("yes")>-1){
+            builder.Prompts.text(session, 'Provide me some  information about your docker environment. Type in whatever you want to');
+            next();
+        }else if(results.response.toLowerCase().indexOf("no")>-1){
+            session.send('Thank you for our interaction. Ping me if you need me later');
+            session.endDialog();
+        }else{
+            session.dialogData.reAskuserResponse = true;
+            session.replaceDialog('/update');
+            return;
+        }
     },
     function (session, results) {
-        session.userData.name = results.response;
+        session.dialogData.updateOne = results.response;
+        builder.Prompts.text(session, 'Thank you. I also need some more information. Type in some more for me please');
+        next();
+    },
+    function(session,results){
+        session.dialogData.updateTwo = results.response;
+        session.send("You provided '"+session.dialogData.updateOne + " "+session.dialogData.updateTwo+ "'. I am going to find out the upgrade recommendation based on your input and will ping you soon");
+        //send call to server
+        //get response from server
+        //session.send response
         session.endDialog();
     }
 ]);
